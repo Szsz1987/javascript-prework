6286e522d3d0dcac6a3b05a46532f826a5df67fe
Szymon Szuliński <sszulinski@gmail.com>
2020-11-25 06:49:43 +0100

    game rock, paper, scissors - defined computer&player moves

--- js/script.js
+++ js/script.js
@@ -1,4 +1,33 @@
-let computerMove = `kamień`;
-let playerMove = `papier`;
+// ruch gracza
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Gracz wpisał: ' + playerInput);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+if(playerInput == '1'){
+    playerMove = 'kamień';
+}
+else if (playerInput == '2'){
+    playerMove = 'papier';
+}
+else {
+    playerMove = 'nożyczki';
+}
+printMessage('Twój ruch to: ' + playerMove);
+
+
+// ruch komputera
+let randomFraction = Math.random();
+printMessage('Wylosowany ułamek to: ' + randomFraction);
+let calculation = randomFraction * 3 + 1;
+printMessage('Ułamek pomnożony przez 3 i powiększony o 1: ' + calculation);
+let roundNumber = Math.floor(calculation);
+printMessage('Liczba po zaokrągleniu w dół to: ' + roundNumber);
+
+if(roundNumber == '1'){
+    computerMove = 'kamień';
+}
+else if (roundNumber == '2'){
+    computerMove = 'papier';
+}
+else if (roundNumber == '3'){
+    computerMove = 'nożyczki';
+}
