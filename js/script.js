841ba5e6ccf0fa28740439e1b82f232bc72a4bb6
Szymon Szuliński <sszulinski@gmail.com>
2020-11-25 08:29:13 +0100

    defined conditional logic of the game

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// ruch komputera
+// computer moves
 let randomFraction = Math.random();
 printMessage('Wylosowany ułamek to: ' + randomFraction);
 let calculation = randomFraction * 3 + 1;
@@ -15,9 +15,10 @@ else if (roundNumber == '2'){
 else if (roundNumber == '3'){
     computerMove = 'nożyczki';
 }
-printMessage('Komputera ruch to: ' + computerMove);
 
-// ruch gracza
+printMessage('Przeciwnika ruch to: ' + computerMove);
+
+// player moves
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 console.log('Gracz wpisał: ' + playerInput);
 
@@ -30,4 +31,27 @@ else if (playerInput == '2'){
 else if (playerInput == '3'){
     playerMove = 'nożyczki';
 }
+else {
+    playerMove = 'nieznany ruch';
+}
 printMessage('Twój ruch to: ' + playerMove);
+
+// duel
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  }
+else if ( computerMove == 'nożyczki' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+  }
+else if ( computerMove == 'papier' && playerMove == 'nożyczki'){
+    printMessage('Ty wygrywasz!');
+}  
+else if( computerMove == playerMove ){
+    printMessage('Remis!');
+  }
+else if( playerMove == 'nieznany ruch'){
+    printMessage('Nieznany ruch!');
+  }  
+else {
+    printMessage ('Przegrałeś!')
+}
