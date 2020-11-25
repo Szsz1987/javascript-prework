620a9d3cd2427edf7361f5ac5ea3d2b847e4d85b
Szymon Szuliński <sszulinski@gmail.com>
2020-11-25 06:58:36 +0100

    game rock.. - moved player moves at the end ofthe code

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-// ruch gracza
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('Gracz wpisał: ' + playerInput);
-
-if(playerInput == '1'){
-    playerMove = 'kamień';
-}
-else if (playerInput == '2'){
-    playerMove = 'papier';
-}
-else {
-    playerMove = 'nożyczki';
-}
-printMessage('Twój ruch to: ' + playerMove);
-
-
 // ruch komputera
 let randomFraction = Math.random();
 printMessage('Wylosowany ułamek to: ' + randomFraction);
@@ -31,3 +15,19 @@ else if (roundNumber == '2'){
 else if (roundNumber == '3'){
     computerMove = 'nożyczki';
 }
+printMessage('Komputera ruch to: ' + computerMove);
+
+// ruch gracza
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Gracz wpisał: ' + playerInput);
+
+if(playerInput == '1'){
+    playerMove = 'kamień';
+}
+else if (playerInput == '2'){
+    playerMove = 'papier';
+}
+else if (playerInput == '3'){
+    playerMove = 'nożyczki';
+}
+printMessage('Twój ruch to: ' + playerMove);
